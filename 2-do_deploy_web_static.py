a3f77201a8a3caa46e72f59d1f2a21755da9b858
Andrew Mukare <dremukare@gmail.com>
2021-08-29 22:12:36 +0000

    created script that distributes archives to web servers

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -4,14 +4,13 @@ fabric script that generates a .tgz archive from
 the contents of web_static
 distributes archive to web servers
 '''
-from fabric.api import *
+from fabric.api import env, put, run
 from datetime import datetime
 from os import path
 
 
-
-env.user = 'ubuntu'
 env.hosts = ['34.75.183.8', '34.139.231.119']
+
 
 def do_deploy(archive_path):
     ''' distributes archive to web servers '''
@@ -20,15 +19,29 @@ def do_deploy(archive_path):
     if path.isfile(archive_path):
         if put(archive_path, '/tmp/{}'.format(file)).failed is True:
             return False
-        if run('tar -xvzf /tmp/{} -C /data/web_static/releases/{}'.format(
-            file, name)).failed is True:
-                return False
-        if sudo('rm /tmp/{}'.format(archive_path)).failed is True:
+        if run('rm -rf /data/web_static/releases/{}/'.
+               format(name)).failed is True:
             return False
-        if sudo('rm /data/web_static/current').failed is True:
+        if run('mkdir -p /data/webstatic/releases/{}/'.
+               format(name)).failed is True:
+            return False
+        if run('tar -xzf /tmp/{} -C /data/web_static/releases/{}'.format(
+                file, name)).failed is True:
+            return False
+        if run('rm /tmp/{}'.format(file)).failed is True:
+            return False
+        if run("mv /data/web_static/releases/{}/web_static/* "
+               "/data/web_static/releases/{}/".format(
+                       name,
+                       name)).failed is True:
+            return False
+        if run("rm -rf /data/web_static/releases/{}/web_static".
+               format(name)).failed is True:
+            return False
+        if run('rm /data/web_static/current').failed is True:
             return False
         if run('ln -s /data/web_static/releases/{} /data/web_static/current'
-            .format(name)).failed is True:
+               .format(name)).failed is True:
             return False
-        return True        
+        return True
     return False
