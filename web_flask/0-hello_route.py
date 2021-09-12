0666b89672b234ae871bc2c2192d290339725336
Andrew Mukare <dremukare@gmail.com>
2021-09-12 17:06:40 +0000

    task 0

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_world():
+    ''' displays 'Hello HBNB' when web app is started '''
+    return 'Hello HBNB!'
+
+
+if __name__ == '__main__':
+    app.run()
