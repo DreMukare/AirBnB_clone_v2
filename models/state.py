a75f7b32f32875408aab8cd22da017fd8fe68060
Andrew Mukare <dremukare@gmail.com>
2021-09-12 22:12:01 +0000

    fixed error

--- models/state.py
+++ models/state.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel, Base
+from models.city import City
 from sqlalchemy.orm import relationship
 from sqlalchemy import Column, String
 import os
@@ -28,7 +29,7 @@ class State(BaseModel, Base):
         def cities(self):
             ''' gets city instances related to current state '''
             city_list = []
-            all_cities = models.storage.all('City')
+            all_cities = models.storage.all(City)
             for city in all_cities.values():
                 if city.state_id == self.id:
                     city_list.append(city)
