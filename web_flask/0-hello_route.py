de10a86d8c927794260455500b130891d194cf60
Andrew Mukare <dremukare@gmail.com>
2021-09-12 17:15:34 +0000

    task 0

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
+''' starts a Flask web app '''
 from flask import Flask
-
-
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
 def hello_world():
-    ''' displays 'Hello HBNB' when web app is started '''
+    """ returns 'Hello HBNB' """
     return 'Hello HBNB!'
 
 
