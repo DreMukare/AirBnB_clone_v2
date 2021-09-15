71529d301a7405a2f1730d5aa8e5094428c52213
Andrew Mukare <dremukare@gmail.com>
2021-09-15 15:26:17 +0300

    Update state.py

--- models/state.py
+++ models/state.py
@@ -1,33 +1,33 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
+import models
 from models.base_model import BaseModel, Base
 from models.city import City
+from os import getenv
+import sqlalchemy
+from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
-from sqlalchemy import Column, String
-import os
-import models
 
 
 class State(BaseModel, Base):
-    """ State class """
-    __tablename__ = 'states'
-    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+    """Representation of state """
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        __tablename__ = 'states'
         name = Column(String(128), nullable=False)
-        cities = relationship("City",
-                              cascade="all, delete",
-                              backref="cities")
+        cities = relationship("City", backref="state", cascade="all, delete")
+
     else:
-        name = ''
+        name = ""
 
     def __init__(self, *args, **kwargs):
-        """initializes class"""
+        """intialize state"""
         super().__init__(*args, **kwargs)
 
-    if os.getenv('HBNB_TYPE_STORAGE') != 'db':
+    if getenv('HBNB_TYPE_STORAGE') != 'db':
 
         @property
         def cities(self):
-            ''' gets city instances related to current state '''
+            """getter for list of city instances related to the state"""
             city_list = []
             all_cities = models.storage.all(City)
             for city in all_cities.values():
